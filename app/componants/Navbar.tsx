fd32f5f475b3297ed9ea45fc8a06fb995a3c1772
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-01-16 19:08:33 +0100

    nav test

--- app/componants/Navbar.tsx
+++ app/componants/Navbar.tsx
@@ -1,39 +1,56 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Search from "./Search";
 import Link from "next/link";
 import { FaHamburger } from "react-icons/fa";
 import { IoIosClose } from "react-icons/io";
+const Nav = [
+  { name: "Home", link: "/" },
+  { name: "Work", link: "/Work" },
+  { name: "Blog", link: "/Blog" },
+  { name: "Contact", link: "/Contact" },
+];
 
-const Navbar = () => {
-  const Nav = [
-    { name: "Home", link: "/" },
-    { name: "Work", link: "/Work" },
-    { name: "Blog", link: "/Blog" },
-    { name: "Contact", link: "/Contact" },
-  ];
+const NavbarDesktop = () => {
   return (
-    <nav className="flex md:justify-between absolute z-10 w-full">
-      <Search />
-      <Link href={"/"}>
-        <h1>ibs</h1>
-      </Link>
-      <ul className="flex md:flex-row md:gap-4  font-bold flex-col justify-around items-center absolute h-screen w-screen z-1500 r-0 bg-white ">
-        <li className="float-right">
-          {" "}
-          <IoIosClose width={50} className="bg-red-500" />{" "}
+    <ul className="none hidden md:flex gap-4">
+      {Nav.map((item, i) => (
+        <li>
+          <Link href={item.link}> {item.name} </Link>
         </li>
-        {Nav.map((v, i) => (
-          <li key={i}>
-            <Link
-              className="flex justify-center items-center h-3 "
-              href={v.link}
-            >
-              {" "}
-              {v.name}{" "}
-            </Link>
+      ))}
+    </ul>
+  );
+};
+
+const NavbarMobile = () => {
+  return (
+    <div>
+      <ul className="md:none md:hidden visible flex flex-col gap-4">
+        {Nav.map((item, i) => (
+          <li>
+            <Link href={item.link}> {item.name} </Link>
           </li>
         ))}
       </ul>
+    </div>
+  );
+};
+const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="flex justify-between absolute z-10 w-full bg-transparent">
+      <Search />
+      <Link href={"/"}>
+        <h1 className="cursor-pointer text-xl font-bold">ibs</h1>
+      </Link>
+      <NavbarDesktop />
+      <NavbarMobile />
     </nav>
   );
 };
